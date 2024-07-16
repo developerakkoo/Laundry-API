3bad7fb2d311b8248ef17c45695f0a16870dc34a
sakibdevlekar <sakibdevlekar96@gmail.com>
2024-07-16 16:33:15 +0530

    socket io added

--- src/index.js
+++ src/index.js
@@ -2,11 +2,38 @@ require("dotenv").config();
 const { app } = require("./app");
 const { connectDB } = require("./db/index.db");
 const PORT = process.env.PORT || 3000;
+let server;
 
 connectDB()
     .then(() => {
-        app.listen(PORT, () => {
-            console.log(`Server is running at port : ${PORT} in ${process.env.NODE_ENV} mode`);
+        server = app.listen(PORT, () => {
+            console.log(
+                `Server is running at port : ${PORT} in ${process.env.NODE_ENV} mode`,
+            );
+        });
+        const io = require("./utils/socket").init(server);
+
+        io.on("connection", async (socket) => {
+            // let { userId } = await jwt.verify(
+            //     socket.handshake.auth.token,
+            //     process.env.JWT_ACCESS_SECRET_KEY,
+            // );
+            console.log("User connected");
+
+            // await User.findByIdAndUpdate(
+            //     userId,
+            //     { $set: { isOnline: true } },
+            //     { new: true },
+            // ); //update user status to online
+            // socket.broadcast.emit("onlineUsers", { user: userId });
+            socket.on("disconnect", async () => {
+                // const userId = socket.handshake.auth.token;
+                // await User.findByIdAndUpdate(userId, {
+                //     $set: { isOnline: false },
+                // }); //update user status to offline
+                // socket.broadcast.emit("offlineUsers", { user: userId });
+                console.log("User Disconnected");
+            });
         });
     })
     .catch((err) => {
---
+++ src/utils/socket.js
@@ -0,0 +1,30 @@
+let io;
+
+module.exports = {
+    init: (httpServer) => {
+        io = require("socket.io")(httpServer, {
+            cors: {
+                origins: [
+                    "http://localhost:8100",
+                    "http://localhost:8200",
+                    "http://localhost:8080",
+                    "http://localhost:8101",
+                    "0.0.0.0",
+                    "*",
+                ],
+                methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+                credentials: true,
+            },
+        });
+
+        return io;
+    },
+
+    getIO: () => {
+        if (!io) {
+            throw new Error("Socket not Initialized");
+        }
+
+        return io;
+    },
+};
