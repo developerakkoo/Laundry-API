c36ecd331c159f006fe91419bd9fefd0678e58d4
sakibdevlekar <sakibdevlekar96@gmail.com>
2024-07-27 15:11:59 +0530

    log added for pm2

--- src/db/index.db.js
+++ src/db/index.db.js
@@ -1,16 +1,21 @@
-const mongoose = require('mongoose');
-const {DB_NAME} = require('../constant');
-const {apiError} = require('../utils/helper.utils');
+const mongoose = require("mongoose");
+const { DB_NAME } = require("../constant");
+const { apiError } = require("../utils/helper.utils");
 
-const connectDB = async () =>{
+const connectDB = async () => {
     try {
-        const connectionInstance =  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        console.log(`MongoDB connected !! DB HOST:${connectionInstance.connection.host}`);
+        console.log(process.env.MONGODB_URI, DB_NAME);
+        const connectionInstance = await mongoose.connect(
+            `${process.env.MONGODB_URI}/${DB_NAME}`,
+        );
+        console.log(
+            `MongoDB connected !! DB HOST:${connectionInstance.connection.host}`,
+        );
     } catch (error) {
-        throw new apiError(500,`MongoDB connection failed ${error.message}`);
+        throw new apiError(500, `MongoDB connection failed ${error.message}`);
     }
-}
+};
 
 module.exports = {
-    connectDB
-}
+    connectDB,
+};
